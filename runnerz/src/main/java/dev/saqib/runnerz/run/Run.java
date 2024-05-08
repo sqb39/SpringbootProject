19e52f496a2e8628abe531fc648b8a96f7e82386
Md Nazmus  Saqib <saqib@Saqibs-MacBook-Air.local>
2024-05-08 16:45:51 +0300

    Includes validation

--- runnerz/src/main/java/dev/saqib/runnerz/run/Run.java
+++ runnerz/src/main/java/dev/saqib/runnerz/run/Run.java
@@ -1,12 +1,24 @@
 package dev.saqib.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Run(
         Integer id,
+        @NotEmpty //Uses spring-boot-starter-validation in dependency
         String title,
         LocalDateTime startedOn,
         LocalDateTime completedOn,
+        @Positive //Uses spring-boot-starter-validation in dependency
         Integer miles,
         Location location
-) {}
+) {
+    //Executes this checking each time during new Run creation
+    public Run{
+        if (!completedOn.isAfter(startedOn)){
+            throw new IllegalArgumentException("Completed On must be after Started On");
+        }
+    }
+}
--- runnerz/src/main/java/dev/saqib/runnerz/run/RunController.java
+++ runnerz/src/main/java/dev/saqib/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package dev.saqib.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -33,13 +34,13 @@ public class RunController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(" ")
-    void create(@RequestBody Run run){
+    void create(@Valid @RequestBody Run run){               //@Valid is used from spring-boot-starter-validation
         runRepository.create(run);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    void update(@RequestBody Run run, @PathVariable Integer id){
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id){
         runRepository.update(run, id);
     }
 
